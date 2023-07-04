a0eb5186a76b4e83c2d98b6b19bef3a4a75b671e
Rod Joost <66536020+rodjoost@users.noreply.github.com>
2023-07-03 22:23:23 -0400

    Delete Ejercicio Individual 4 directory

--- Ejercicio Individual 4/src/aplicacion/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.inicio, name='index'),
-    path('usuarios/', views.lista_usuarios, name='lista_usuarios'),
-    path('inicio/', views.inicio, name='index'),
-    path('registro/', views.registro, name='registro'), 
-]
